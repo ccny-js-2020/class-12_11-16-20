9e425d2654a978bd233dfef01cccd3814362dd5e
Jared Thomas <jaredthomas@Jareds-MacBook-Air.local>
2020-11-16 20:23:24 -0500

    adding lessons and unsolved exercises

---
+++ jquery_data/data_js/easy-exercise/unsolved/set_data.js
@@ -0,0 +1,13 @@
+/*
+	- Create an object array out of some of your favorite tv shows, at least 3
+		- Give the object showName, yearsRun, and premise properties and provide the values of those for each of the tv shows
+	- Create buttons for each of the shows in the object array
+		- Give each button a text of the tv show name
+		- Give each button a data value of the tv show name, setting it with jquery data
+		- Append all buttons to the dom
+	- When any button is clicked on
+		- It will clear out any previous html in the explanations-div
+		- It will display which show was clicked on in the explanations-div
+		- It will display the years that the show ran in the explanations-div
+		- It will display the premise of the show in the explanations-div
+*/
